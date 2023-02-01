4c40a7818eac83a81ebbddbcad908efbebd1ecff
Peter Falz <Peter.Falz@abatplus.de>
2023-02-01 15:38:33 +0100

    Version 3.0.1 - finalize #1

--- lib/app.ts
+++ lib/app.ts
@@ -4,8 +4,7 @@ import express from 'express';
 import path from 'path';
 import { processenv } from 'processenv';
 
-
-// Server 03 ... Postgres-Gedöhns
+// Default-Output Start
 (async () => {
   const client = new Client({
     host: 'dc-db',              // --- network-alias aus run-Command
@@ -25,13 +24,44 @@ import { processenv } from 'processenv';
   await client.end();
 })();
 
-/*
 
+// Server 03 ... Postgres-Gedöhns
+const server03 = http.createServer((req, res) => {
+  var msg = '######';
+
+  (async () => {
+    const client = new Client({
+      host: 'dc-db',              // --- network-alias aus run-Command
+      port: 5432,                 // --- Standard-Port
+      user: 'postgres',           // --- Standard-User
+      password: 'secret',         // --- POSTGRES_PASSWORD aus run-Command
+      database: 'postgres'        // --- Standard-DB
+    });
+  
+    await client.connect();
+    const { rows } = await client.query(
+      'SELECT $1::text AS message', [ 'Hallo aus PostgreSQL! *']
+    );
+  
+    msg = rows[0].message;
+
+    await client.end();
+  
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write( msg + '\n' );
+    res.end();
+  })();
+});
+
+server03.listen(3003);
+
+// /*
 // Server 02 ... Express
 const api = express();
 api.use('/', express.static(path.join(__dirname, '..', 'public')));
 const server02 = http.createServer(api);
 server02.listen(3002);
+// */
 
 // /*
 // Server 01 ... Umgebungsvariable
@@ -45,5 +75,4 @@ const server01 = http.createServer((req, res) => {
 });
 
 server01.listen(3001);
-
-*/
+// */

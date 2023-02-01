828acefdc4c8504ef523c705c7399ee54c302f4d
Peter Falz <Peter.Falz@abatplus.de>
2023-02-01 14:15:16 +0100

    Version 3.0.0 - appending PostgreSQL

--- lib/app.ts
+++ lib/app.ts
@@ -1,10 +1,31 @@
+import { Client } from 'pg';
 import http from 'http';
-
 import express from 'express';
 import path from 'path';
-
 import { processenv } from 'processenv';
 
+
+// Server 03 ... Postgres-Gedöhns
+(async () => {
+  const client = new Client({
+    host: 'dc-db',              // --- network-alias aus run-Command
+    port: 5432,                 // --- Standard-Port
+    user: 'postgres',           // --- Standard-User
+    password: 'secret',         // --- POSTGRES_PASSWORD aus run-Command
+    database: 'postgres'        // --- Standard-DB
+  });
+
+  await client.connect();
+  const { rows } = await client.query(
+    'SELECT $1::text AS message', [ 'Hallo aus PostgreSQL! #']
+  );
+
+  console.log( rows[0].message );
+
+  await client.end();
+})();
+
+/*
 
 // Server 02 ... Express
 const api = express();
@@ -18,10 +39,11 @@ const message = processenv('MESSAGE', 'Hello World! #');
 const server01 = http.createServer((req, res) => {
   //res.statusCode = 200;
   //res.setHeader('Content-Type', 'text/plain');
-  res.writeHead(200, {'Content-Type': 'text/phtml'});
+  res.writeHead(200, {'Content-Type': 'text/html'});
   res.write( message + '\n' );
   res.end();
 });
 
 server01.listen(3001);
-// */
+
+*/
